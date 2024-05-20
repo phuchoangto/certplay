3a57364a2a0566cbba9ea71e34f315023970e576
Phuc To <me@phuc.to>
2024-05-20 21:08:29 +0700

    refactor: Update BASE_URL to use 'http://homeserver:3000' for home server configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+      baseUrl: 'http://homeserver:3000',
     },
   },
   supabase: {
